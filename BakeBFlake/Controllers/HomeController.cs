aa28b1daf06b3c3b8766d0ce30e9d7641fab3321
Maayan Keenan <maayankeen@gmail.com>
2014-09-28 23:10:42 +0300

    search page functionality

--- BakeBFlake/Controllers/HomeController.cs
+++ BakeBFlake/Controllers/HomeController.cs
@@ -27,6 +27,26 @@ namespace BakeBFlake.Controllers
             return View(model);
         }
 
+        public PartialViewResult Search(string name, string type, string price, bool? glotanFree, bool? vegan)
+        {
+            var model = new System.Collections.Generic.List<Pastery>();
+            if (name != null)
+            {
+                model.Add(new Pastery() { Id = 1, Name = "Whole Wheat Bread", Price = 9.99, ImageLink = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS1lnOLjYuqp5hcUoZv2zp3-Hy1qkrMBHxJAGMZS7FUh6KP9UdFaA" });
+                model.Add(new Pastery() { Id = 2, Name = "Rye Bread", Price = 9.99, ImageLink = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSgpMMudTEPY9GQUFFXNi1aAkM2spjZ9lT-zqsLynF1rMULXnkG" });
+            }
+            else
+            {
+                model.Add(new Pastery() { Id = 1, Name = "Whole Wheat Bread", Price = 9.99, ImageLink = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcS1lnOLjYuqp5hcUoZv2zp3-Hy1qkrMBHxJAGMZS7FUh6KP9UdFaA" });
+                model.Add(new Pastery() { Id = 2, Name = "Rye Bread", Price = 9.99, ImageLink = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcSgpMMudTEPY9GQUFFXNi1aAkM2spjZ9lT-zqsLynF1rMULXnkG" });
+                model.Add(new Pastery() { Id = 3, Name = "Vanilla Cupcakes", Price = 9.99, ImageLink = "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcS5oatXxYiBZLT7m5DnDnJ-fXeeO75yIec6Fdepcjlf2QWKc1F1nA" });
+                model.Add(new Pastery() { Id = 4, Name = "Choclate Cake", Price = 9.99, ImageLink = "https://encrypted-tbn1.gstatic.com/images?q=tbn:ANd9GcSnkc1WbBgCDbnWka_wOzg9lanUb3eRDR2G3pS_iGIYdBp8bPiH" });
+                model.Add(new Pastery() { Id = 5, Name = "Onion Bagel", Price = 9.99, ImageLink = "https://encrypted-tbn3.gstatic.com/images?q=tbn:ANd9GcRhbizKQJIr3dqooIqfeq6mmbNrvvLeNp-PmfJd6de6jPsQvExqjA" });
+            }
+
+            return PartialView(model);
+        }
+
         // order/types/{type}
         public ActionResult Types(string type)
         {
