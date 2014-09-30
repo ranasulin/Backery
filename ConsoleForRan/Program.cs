18705d7424d5db4b472e66ed9de7801eea432e68
Ido Ganzer <iganzer@utest.com>
2014-09-30 22:42:16 +0300

    Console for ran

---
+++ ConsoleForRan/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleForRan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            String s = "עידו נאור מרצה מספר 1";
+        }
+    }
+}
